d2fcfc0094bf74dc591dbb026790a0276ceb9ad3
Koy <369491420@qq.com>
2020-08-29 12:13:38 +0800

    [refactor] line.go done ! (#1)
    * [refactor] line.go
    
    * [update] correct Shop Name from ShopA to ShopB.
    
    * [refine] refine descriptions CN to EN.

---
+++ line.go
@@ -0,0 +1,269 @@
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"os"
+
+	"github.com/go-echarts/go-echarts/charts"
+	"github.com/go-echarts/go-echarts/components"
+	"github.com/go-echarts/go-echarts/opts"
+)
+
+var (
+	itemCntLine = 6
+	nameItems   = []string{"Apple", "Banana", "Peach ", "Lemon", "Pear", "Cherry"}
+)
+
+func generateLineItems() []opts.LineData {
+	items := make([]opts.LineData, 0)
+	for i := 0; i < itemCntLine; i++ {
+		items = append(items, opts.LineData{Value: rand.Intn(300)})
+	}
+	return items
+}
+
+func generateLineData(data []float32) []opts.LineData {
+	items := make([]opts.LineData, 0)
+	for i := 0; i < len(data); i++ {
+		items = append(items, opts.LineData{Value: data[i]})
+	}
+	return items
+}
+
+func lineBase() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title:    "Line-basic-example",
+			Subtitle: "This is the subtitle.",
+		}),
+	)
+
+	line.SetXAxis(nameItems).
+		AddSeries("Category A", generateLineItems())
+	return line
+}
+
+func lineShowLabel() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title:    "Line-with-title/label-opts",
+			Subtitle: "go-echarts is an awesome chart library written in Golang",
+			Link:     "https://github.com/go-echarts/go-echarts",
+		}),
+	)
+
+	line.SetXAxis(nameItems).
+		AddSeries("Category A", generateLineItems()).
+		SetSeriesOptions(
+			charts.WithLabelOpts(opts.Label{
+				Show: true,
+			}),
+		)
+	return line
+}
+
+func lineMarkPoint() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Line-markpoint",
+		}),
+	)
+
+	line.SetXAxis(nameItems).AddSeries("Category A", generateLineItems()).
+		SetSeriesOptions(
+			charts.WithMarkPointNameTypeItemOpts(
+				opts.MarkPointNameTypeItem{Name: "Maximum", Type: "max"},
+				opts.MarkPointNameTypeItem{Name: "Average", Type: "average"},
+				opts.MarkPointNameTypeItem{Name: "Minimum", Type: "min"},
+			),
+			charts.WithMarkPointStyleOpts(
+				opts.MarkPointStyle{Label: &opts.Label{Show: true}}),
+		)
+	return line
+}
+
+func lineSplitLine() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Line-splitline",
+		}),
+		charts.WithYAxisOpts(opts.YAxis{
+			SplitLine: &opts.SplitLine{
+				Show: true,
+			},
+		}),
+	)
+
+	line.SetXAxis(nameItems).AddSeries("Category A", generateLineItems(),
+		charts.WithLabelOpts(
+			opts.Label{Show: true},
+		))
+	return line
+}
+
+func lineStep() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Line-step",
+		}),
+	)
+
+	line.SetXAxis(nameItems).AddSeries("Category A", generateLineItems()).
+		SetSeriesOptions(charts.WithLineChartOpts(
+			opts.LineChart{
+				Step: true,
+			}),
+		)
+	return line
+}
+
+func lineSmooth() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Line-smooth",
+		}),
+	)
+
+	line.SetXAxis(nameItems).AddSeries("Category A", generateLineItems()).
+		SetSeriesOptions(charts.WithLineChartOpts(
+			opts.LineChart{
+				Smooth: true,
+			}),
+		)
+	return line
+}
+
+func lineArea() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Line-area",
+		}),
+	)
+
+	line.SetXAxis(nameItems).AddSeries("Category A", generateLineItems()).
+		SetSeriesOptions(
+			charts.WithLabelOpts(
+				opts.Label{
+					Show: true,
+				}),
+			charts.WithAreaStyleOpts(
+				opts.AreaStyle{
+					Opacity: 0.2,
+				}),
+		)
+	return line
+}
+
+func lineSmoothArea() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Line-smooth-area",
+		}),
+	)
+
+	line.SetXAxis(nameItems).AddSeries("Category A", generateLineItems()).
+		SetSeriesOptions(
+			charts.WithLabelOpts(opts.Label{
+				Show: true,
+			}),
+			charts.WithAreaStyleOpts(opts.AreaStyle{
+				Opacity: 0.2,
+			}),
+			charts.WithLineChartOpts(opts.LineChart{
+				Smooth: true,
+			}),
+		)
+	return line
+}
+
+func lineMulti() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Line-multi-lines",
+		}),
+		charts.WithInitializationOpts(opts.Initialization{
+			Theme: "shine",
+		}),
+	)
+
+	line.SetXAxis(nameItems).
+		AddSeries("Category  A", generateLineItems()).
+		AddSeries("Category  B", generateLineItems()).
+		AddSeries("Category  C", generateLineItems()).
+		AddSeries("Category  D", generateLineItems())
+	return line
+}
+
+func lineDemo() *charts.Line {
+	line := charts.NewLine()
+
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Search Time: Hash table vs Binary search",
+		}),
+		charts.WithYAxisOpts(opts.YAxis{
+			Name: "Cost time(ns)",
+			SplitLine: &opts.SplitLine{
+				Show: false,
+			},
+		}),
+		charts.WithXAxisOpts(opts.XAxis{
+			Name: "Elements",
+		}),
+	)
+
+	line.SetXAxis([]string{"10e1", "10e2", "10e3", "10e4", "10e5", "10e6", "10e7"}).
+		AddSeries("map", generateLineItems(),
+			charts.WithLabelOpts(opts.Label{Show: true, Position: "bottom"})).
+		AddSeries("slice", generateLineData([]float32{24.9, 34.9, 48.1, 58.3, 69.7, 123, 131}),
+			charts.WithLabelOpts(opts.Label{Show: true, Position: "top"})).
+		SetSeriesOptions(
+			charts.WithMarkLineNameTypeItemOpts(opts.MarkLineNameTypeItem{
+				Name: "Average",
+				Type: "average",
+			}),
+			charts.WithLineChartOpts(opts.LineChart{
+				Smooth: true,
+			}),
+			charts.WithMarkPointStyleOpts(opts.MarkPointStyle{
+				Label: &opts.Label{
+					Show:      true,
+					Formatter: "{a}: {b}",
+				},
+			}),
+		)
+
+	return line
+}
+
+func main() {
+	page := components.NewPage()
+	page.AddCharts(
+		lineBase(),
+		lineShowLabel(),
+		lineMarkPoint(),
+		lineSplitLine(),
+		lineStep(),
+		lineSmooth(),
+		lineArea(),
+		lineSmoothArea(),
+		lineMulti(),
+		lineDemo(),
+	)
+	f, err := os.Create("line.html")
+	if err != nil {
+		log.Println(err)
+	}
+	_ = page.Render(io.MultiWriter(os.Stdout, f))
+}
