77f84eeae3d1e7317f8a736f05bf7416ae7875b3
Koy <369491420@qq.com>
2020-09-04 20:45:56 +0800

    format

--- gauge.go
+++ gauge.go
@@ -17,6 +17,7 @@ func gaugeBase() *charts.Gauge {
 	gauge.SetGlobalOptions(
 		charts.WithTitleOpts(opts.Title{Title: "Gauge-example"}),
 	)
+
 	gauge.AddSeries("ProjectA", []opts.GaugeData{{Name: "Work progress", Value: rand.Intn(50)}})
 	return gauge
 }
@@ -26,7 +27,9 @@ func gaugeTimer() *charts.Gauge {
 	gauge.SetGlobalOptions(
 		charts.WithTitleOpts(opts.Title{Title: "Gauge-timer"}),
 	)
+
 	gauge.AddSeries("ProjectB", []opts.GaugeData{{Name: "Work progress", Value: rand.Intn(50)}})
+
 	fn := fmt.Sprintf(`setInterval(function () {
 			option_%s.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
 			myChart_%s.setOption(option_%s, true);
