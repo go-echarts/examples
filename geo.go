ce97c2534d9490ce76b9832c1e5d523e1653ac9b
Koy <koy@ko8e24.top>
2020-09-26 18:04:11 +0800

    refactor geo.go

---
+++ geo.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"github.com/go-echarts/go-echarts/charts"
+	"github.com/go-echarts/go-echarts/components"
+	"github.com/go-echarts/go-echarts/opts"
+	"io"
+	"log"
+	"math/rand"
+	"os"
+)
+
+var geoData = []opts.GeoData{
+	{"北京", []float64{116.40, 39.90, float64(rand.Intn(100))}},
+	{"上海", []float64{121.47, 31.23, float64(rand.Intn(100))}},
+	{"重庆", []float64{106.55, 29.56, float64(rand.Intn(100))}},
+	{"武汉", []float64{114.31, 30.52, float64(rand.Intn(100))}},
+	{"台湾", []float64{121.30, 25.03, float64(rand.Intn(100))}},
+	{"香港", []float64{114.17, 22.28, float64(rand.Intn(100))}},
+}
+
+var guangdongData = []opts.GeoData{
+	{"汕头", []float64{116.69, 23.39, float64(rand.Intn(100))}},
+	{"深圳", []float64{114.07, 22.62, float64(rand.Intn(100))}},
+	{"广州", []float64{113.23, 23.16, float64(rand.Intn(100))}},
+}
+
+func geoBase() *charts.Geo {
+	geo := charts.NewGeo()
+	geo.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "geo-effectScatter",
+		}),
+		charts.WithGeoComponentOpts(opts.GeoComponent{
+			Map: "china",
+			ItemStyle: opts.ItemStyle{
+				Color: "#006666",
+			},
+		}),
+	)
+
+	geo.AddSeries("geo", "effectScatter", geoData,
+		charts.WithRippleEffectOpts(opts.RippleEffect{
+			Period:    4,
+			Scale:     6,
+			BrushType: "stroke",
+		}),
+	)
+	return geo
+}
+
+func geoGuangdong() *charts.Geo {
+	geo := charts.NewGeo()
+	geo.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "geo-Guangdong",
+		}),
+		charts.WithGeoComponentOpts(opts.GeoComponent{
+			Map: "广东",
+		}),
+		charts.WithVisualMapOpts(opts.VisualMap{
+			Calculable: true,
+			InRange: &opts.VisualMapInRange{
+				Color: []string{"#50a3ba", "#eac736", "#d94e5d"},
+			},
+		}),
+	)
+
+	geo.AddSeries("geo", "scatter", guangdongData)
+	return geo
+}
+
+func main() {
+	page := components.NewPage()
+	page.AddCharts(
+		geoBase(),
+		geoGuangdong(),
+	)
+
+	f, err := os.Create("geo.html")
+	if err != nil {
+		log.Println(err)
+	}
+	_ = page.Render(io.MultiWriter(os.Stdout, f))
+}
