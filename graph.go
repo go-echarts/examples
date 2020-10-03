3e8e968d93481df406fc8346cd82843bd86989c0
Koy <koy@ko8e24.top>
2020-10-03 10:28:47 +0800

    [refactor] graph.go.

---
+++ graph.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/go-echarts/go-echarts/charts"
+	"github.com/go-echarts/go-echarts/components"
+	"github.com/go-echarts/go-echarts/opts"
+)
+
+var graphNodes = []opts.GraphNode{
+	{Name: "Node1"},
+	{Name: "Node2"},
+	{Name: "Node3"},
+	{Name: "Node4"},
+	{Name: "Node5"},
+	{Name: "Node6"},
+	{Name: "Node7"},
+	{Name: "Node8"},
+}
+
+func genLinks() []opts.GraphLink {
+	links := make([]opts.GraphLink, 0)
+	for i := 0; i < len(graphNodes); i++ {
+		for j := 0; j < len(graphNodes); j++ {
+			links = append(links,
+				opts.GraphLink{Source: graphNodes[i].Name, Target: graphNodes[j].Name})
+		}
+	}
+	return links
+}
+
+func graphBase() *charts.Graph {
+	graph := charts.NewGraph()
+	graph.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Graph-basic-example",
+		}))
+
+	graph.AddSeries("graph", graphNodes, genLinks(),
+		charts.WithGraphChartOpts(
+			opts.GraphChart{
+				Force: opts.GraphForce{
+					Repulsion: 8000,
+				},
+			}),
+	)
+	return graph
+}
+
+func graphCircle() *charts.Graph {
+	graph := charts.NewGraph()
+	graph.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Graph-layout-Circular",
+		}))
+
+	graph.AddSeries("graph", graphNodes, genLinks()).
+		SetSeriesOptions(
+			charts.WithGraphChartOpts(
+				opts.GraphChart{
+					Force: opts.GraphForce{
+						Repulsion: 8000,
+					},
+					Layout: "circular",
+				}),
+
+			charts.WithLabelOpts(opts.Label{
+				Show:     true,
+				Position: "right",
+			}),
+		)
+	return graph
+}
+
+func graphNpmDep() *charts.Graph {
+	graph := charts.NewGraph()
+	graph.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Graph-demo-npm-dependencies",
+		}))
+
+	f, err := ioutil.ReadFile("examples/fixtures/npmdepgraph.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type Data struct {
+		Nodes []opts.GraphNode
+		Links []opts.GraphLink
+	}
+
+	var data Data
+	if err := json.Unmarshal(f, &data); err != nil {
+		fmt.Println(err)
+	}
+
+	graph.AddSeries("graph", data.Nodes, data.Links).
+		SetSeriesOptions(
+			charts.WithGraphChartOpts(opts.GraphChart{
+				Layout:             "none",
+				Roam:               true,
+				FocusNodeAdjacency: true,
+			}),
+			charts.WithEmphasisOpts(opts.Emphasis{
+				Label: opts.Label{
+					Show:     true,
+					Color:    "black",
+					Position: "left",
+				},
+			}),
+			charts.WithLineStyleOpts(opts.LineStyle{
+				Curveness: 0.3,
+			}),
+		)
+	return graph
+}
+
+func main() {
+	page := components.NewPage()
+	page.AddCharts(
+		graphBase(),
+		graphCircle(),
+		graphNpmDep(),
+	)
+
+	f, err := os.Create("graph.html")
+	if err != nil {
+		log.Println(err)
+
+	}
+	_ = page.Render(io.MultiWriter(os.Stdout, f))
+
+}
