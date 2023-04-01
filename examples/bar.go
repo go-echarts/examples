ddc8767bb09afa8b65b0b36c4cb1bd99be379f40
Koy ['kɔɪ] <koy@ko8e24.top>
2023-04-01 20:22:53 +0800

    update: update base dependency version and fix demos. (#34)

--- examples/bar.go
+++ examples/bar.go
@@ -45,7 +45,7 @@ func barTitle() *charts.Bar {
 			Right:    "40%",
 		}),
 		charts.WithToolboxOpts(opts.Toolbox{Show: true}),
-		charts.WithLegendOpts(opts.Legend{Right: "80%"}),
+		charts.WithLegendOpts(opts.Legend{Show: true, Right: "80%"}),
 	)
 	bar.SetXAxis(weeks).
 		AddSeries("Category A", generateBarItems()).
@@ -58,7 +58,7 @@ func barTooltip() *charts.Bar {
 	bar.SetGlobalOptions(
 		charts.WithTitleOpts(opts.Title{Title: "tooltip options"}),
 		charts.WithTooltipOpts(opts.Tooltip{Show: true}),
-		charts.WithLegendOpts(opts.Legend{Right: "80%"}),
+		charts.WithLegendOpts(opts.Legend{Show: true, Right: "80%"}),
 	)
 	bar.SetXAxis(weeks).
 		AddSeries("Category A", generateBarItems()).
